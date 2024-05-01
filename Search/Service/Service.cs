4736becc2e803667510213d2b3a0fe99a6d82371
DKV1100 <khanhvan.dkv@gmail.com>
2024-05-01 22:32:00 +0700

    fix: edit input binary tree

--- Search/Service/Service.cs
+++ Search/Service/Service.cs
@@ -24,23 +24,53 @@ namespace Search.Service
                 Console.WriteLine("String found at position " + result);
         }
 
+        //public async Task RunBinaryTree()
+        //{
+        //    BinaryTree tree = new BinaryTree();
+
+        //    /* Let's create a tree with nodes
+        //                  50
+        //                 /  \
+        //                30   70
+        //               / \   / \
+        //             20  40 60 80 */
+        //    tree.Insert(50);
+        //    tree.Insert(30);
+        //    tree.Insert(20);
+        //    tree.Insert(40);
+        //    tree.Insert(70);
+        //    tree.Insert(60);
+        //    tree.Insert(80);
+
+        //    // Print inorder traversal of the BST
+        //    Console.WriteLine("Inorder traversal of the binary tree is ");
+        //    tree.Inorder();
+
+        //    // Prompt user to enter the key to search
+        //    Console.Write("\nEnter the key to search: ");
+        //    int keyToSearch = int.Parse(Console.ReadLine());
+
+        //    if (tree.Search(keyToSearch))
+        //        Console.WriteLine($"\nKey {keyToSearch} found in the binary tree.");
+        //    else
+        //        Console.WriteLine($"\nKey {keyToSearch} not found in the binary tree.");
+        //}
         public async Task RunBinaryTree()
         {
             BinaryTree tree = new BinaryTree();
 
-            /* Let's create a tree with nodes
-                          50
-                         /  \
-                        30   70
-                       / \   / \
-                     20  40 60 80 */
-            tree.Insert(50);
-            tree.Insert(30);
-            tree.Insert(20);
-            tree.Insert(40);
-            tree.Insert(70);
-            tree.Insert(60);
-            tree.Insert(80);
+            Console.WriteLine("Enter the values to insert into the binary tree (enter -1 to stop):");
+
+            int value;
+            do
+            {
+                Console.Write("Enter a value (-1 to stop): ");
+                value = int.Parse(Console.ReadLine());
+
+                if (value != -1)
+                    tree.Insert(value);
+
+            } while (value != -1);
 
             // Print inorder traversal of the BST
             Console.WriteLine("Inorder traversal of the binary tree is ");
